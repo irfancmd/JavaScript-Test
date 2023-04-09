1cc7ef42e8a7999d36b2449d076b85413ff0c81d
Chowdhury Mohammad Irfanuddin <irfanuddincmd@gmail.com>
2023-04-09 19:59:21 +0600

    Jest: Basics contd...

--- Testing/Jest/script.js
+++ Testing/Jest/script.js
@@ -1,7 +1,20 @@
+// We can run jest with --coverage argument to see how much of the code is covered by test
+// Doing so will generate a coverage report
+
 function sum(a, b) {
     return a + b;
 }
 
+function getMax(arr) {
+    if(arr.length === 0) {
+        return null;
+    }
+
+    return Math.max(...arr);
+}
+
+
 module.exports = {
-    sum
+    sum,
+    getMax
 };
--- Testing/Jest/script.test.js
+++ Testing/Jest/script.test.js
@@ -1,8 +1,26 @@
-const { sum } = require("./script");
+const { sum, getMax } = require("./script");
 
-test("It adds two numbers", () => {
-    const a = 1;
-    const b = 2;
+describe("#sum", () => {
+    test("It adds two numbers", () => {
+        const a = 1;
+        const b = 2;
 
-    expect(sum(a, b)).toBe(3);
+        // toBe() checks for same reference. toEqual() checks for same value
+        expect(sum(a, b)).toBe(3);
+    })
 });
+
+describe("#getMax", () => {
+    test("It returns the maximum value of an array", () => {
+        const arr = [1, 3, 7, 4, 5];
+
+        expect(getMax(arr)).toBe(7);
+    })
+
+    test("It checks whether getMax returns null for empty array", () => {
+            const arr = [];
+
+            expect(getMax(arr)).toBeNull();
+    })
+});
+
